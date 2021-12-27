070fcd8b376f48f2e5f2b9ccaef669b0353bd4c0
Nick Granados <internick2017@gmail.com>
2021-12-27 16:23:11 -0300

    importaciones y exportaciones

---
+++ src/bases/08-imp-exp.js
@@ -0,0 +1,33 @@
+//  Import, export y funciones comunes de arreglos
+// import { heroes } from "./data/heroes";
+// import {heroes} from './data/heroes'
+// import { heroes } from './data/heroes'
+// import cualquierCosaArr from './data/heroes'
+import  heroes, {owners}  from '../data/heroes'
+// import  {heroes, owners}  from './data/heroes'
+
+console.log(owners);
+
+// const getHeroeById = (id) => {
+//     return heroes.find((heroe) => {
+//         if (heroe.id === id) {
+//             return true;
+//         } else {
+//             return false
+//         }
+//     });
+// }
+
+// const getHeroeById = (id) => {
+//     return heroes.find( (heroe) => heroe.id === id);
+// }
+
+
+const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+
+console.log(getHeroeById(5));
+
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+
+console.log(getHeroesByOwner('Marvel')); 
